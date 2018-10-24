748568b3b7a9c2c9b85f9efdc0332d3da33504ad
Parham Alvani <parham.alvani@gmail.com>
2018-10-25 00:54:13 +0330

    Lints the file.

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,18 +1,18 @@
 // Core Module
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule, Title }    from '@angular/platform-browser';
-import { AppRoutingModule }        from './app-routing.module';
+import { BrowserModule, Title } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgModule }    from '@angular/core';
+import { NgModule } from '@angular/core';
 import * as global from './globals';
 
 // Main Component
-import { AppComponent }          from './app.component';
-import { HeaderComponent }       from './header/header.component';
-import { SidebarComponent }      from './sidebar/sidebar.component';
-import { TopMenuComponent }      from './top-menu/top-menu.component';
-import { FooterComponent }       from './footer/footer.component';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 import { LoginPage } from './pages/login/login';
 import { RegisterPage } from './pages/register/register';
@@ -42,7 +42,7 @@ export class AppModule {
   constructor(private router: Router, private titleService: Title, private route: ActivatedRoute) {
     router.events.subscribe((e) => {
       if (e instanceof NavigationEnd) { // set page tite based on `title` data of current route
-        var title = 'I1820 | ' + this.route.snapshot.firstChild.data['title'];
+        const title = 'I1820 | ' + this.route.snapshot.firstChild.data['title'];
         this.titleService.setTitle(title);
       }
     });
