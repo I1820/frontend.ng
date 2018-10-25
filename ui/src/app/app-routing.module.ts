144e94a4f8886865f48b945dc5d9b27bacb057f7
Parham Alvani <parham.alvani@gmail.com>
2018-10-25 07:55:00 +0330

    Adds routing module.

---
+++ ui/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+// User Login / Register
+import { LoginPage } from './pages/login/login';
+import { RegisterPage } from './pages/register/register';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // by default client is redirected to login page.
+
+  { path: 'login', component: LoginPage, data: { title: 'Login' } },
+  { path: 'register', component: RegisterPage, data: { title: 'Register' } },
+];
+
+@NgModule({
+  imports: [ CommonModule, RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ],
+  declarations: []
+})
+
+
+export class AppRoutingModule { }
