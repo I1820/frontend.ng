1212bfb4bb88f6c94976835a06f4613ce18e9fce
Parham Alvani <parham.alvani@gmail.com>
2018-10-26 13:18:12 +0330

    Adds validation for register page.

--- ui/src/app/pages/register/register.component.ts
+++ ui/src/app/pages/register/register.component.ts
@@ -1,24 +1,49 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { NgForm } from '@angular/forms';
+import { NgForm, NgModel } from '@angular/forms';
 import { AppComponent } from '../../app.component';
 import { AuthenticationService } from '../../shared';
 
 @Component({
-    selector: 'register',
-    templateUrl: './register.component.html'
+  selector: 'register',
+  templateUrl: './register.component.html'
 })
 
 export class RegisterComponent {
+  private loading: boolean;
+
   constructor(private app: AppComponent, private router: Router, private authService: AuthenticationService) {
-      app.setPageSettings({
-        pageEmpty: true,
-        pageBodyWhite: true
-      });
+    this.loading = false;
+    app.setPageSettings({
+      pageEmpty: true,
+      pageBodyWhite: true
+    });
   }
 
-  formSubmit(f: NgForm) {
-    this.authService.signup(f.value.username)
+  public get submitButtonText(): string {
+    if (this.loading) {
+      return `<i class="fas fa-spinner fa-spin"></i>`
+    } else {
+      return "Sign Up"
+    }
+  }
+
+  public modelValidationStatusClass(m: NgModel): string {
+    if (m.invalid && (m.dirty || m.touched)) {
+      return "is-invalid"
+    }
+    return ""
+  }
+
+  public formSubmit(f: NgForm) {
+    this.loading = true;
+    this.authService.signup(f.value).subscribe(() => {
+      this.loading = false;
+      console.log('register')
+    }, (err) => {
+      this.loading = false;
+      console.log(err)
+    })
     // this.router.navigate(['dashboard']);
   }
 }
