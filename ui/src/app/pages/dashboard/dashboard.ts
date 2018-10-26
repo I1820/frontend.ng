edc410209e3e13cef060f9d885ab364cce27a67f
Parham Alvani <parham.alvani@gmail.com>
2018-10-27 00:03:32 +0330

    Adds empty dashboard.

---
+++ ui/src/app/pages/dashboard/dashboard.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import * as global from '../../globals';
+
+@Component({
+  selector: 'dashboard',
+  templateUrl: './dashboard.html',
+  styleUrls: [
+  ],
+  encapsulation: ViewEncapsulation.None
+})
+
+export class DashboardPage implements OnInit {
+
+  ngOnInit() {
+  }
+
+}
