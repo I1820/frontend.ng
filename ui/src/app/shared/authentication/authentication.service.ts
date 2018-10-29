1ab5eedc22a3586d032b8e14e3cd456f2c9e0d43
Parham Alvani <parham.alvani@gmail.com>
2018-10-29 10:39:31 +0330

    Corrects authentication lint errors.

--- ui/src/app/shared/authentication/authentication.service.ts
+++ ui/src/app/shared/authentication/authentication.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { tap, map, switchMap, catchError } from 'rxjs/operators';
 import { AuthService } from 'ngx-auth';
 
@@ -29,8 +29,6 @@ export class AuthenticationService implements AuthService {
   /**
    * Check, if user already authorized.
    * @description Should return Observable with true or false values
-   * @returns {Observable<boolean>}
-   * @memberOf AuthService
    */
   public isAuthorized(): Observable < boolean > {
     return this.tokenStorage
@@ -42,7 +40,6 @@ export class AuthenticationService implements AuthService {
    * Get access token
    * @description Should return access token in Observable from e.g.
    * localStorage
-   * @returns {Observable<string>}
    */
   public getAccessToken(): Observable < string > {
     return this.tokenStorage.getAccessToken();
@@ -52,7 +49,6 @@ export class AuthenticationService implements AuthService {
    * Function, that should perform refresh token verifyTokenRequest
    * @description Should be successfully completed so interceptor
    * can execute pending requests or retry original one
-   * @returns {Observable<any>}
    */
   public refreshToken(): Observable <AccessData> {
     return this.tokenStorage
@@ -65,7 +61,7 @@ export class AuthenticationService implements AuthService {
       catchError((err) => {
         this.logout();
 
-        return Observable.throw(err);
+        return throwError(err);
       })
     );
   }
@@ -74,18 +70,14 @@ export class AuthenticationService implements AuthService {
    * Function, checks response of failed request to determine,
    * whether token be refreshed or not.
    * @description Essentialy checks status
-   * @param {Response} response
-   * @returns {boolean}
    */
   public refreshShouldHappen(response: HttpErrorResponse): boolean {
-    return response.status === 401
+    return response.status === 401;
   }
 
   /**
    * Verify that outgoing request is refresh-token,
    * so interceptor won't intercept this request
-   * @param {string} url
-   * @returns {boolean}
    */
   public verifyTokenRequest(url: string): boolean {
     return url.endsWith('/refresh');
@@ -121,9 +113,6 @@ export class AuthenticationService implements AuthService {
 
   /**
    * Save access data in the storage
-   *
-   * @private
-   * @param {AccessData} data
    */
   private saveAccessData({ accessToken, refreshToken }: AccessData) {
     this.tokenStorage
