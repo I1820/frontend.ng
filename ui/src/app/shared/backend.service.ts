89eee167c62600fe915f80dcea446ba7257afe4b
Parham Alvani <parham.alvani@gmail.com>
2018-10-30 18:40:16 +0330

    Handles backend errors.

--- ui/src/app/shared/backend.service.ts
+++ ui/src/app/shared/backend.service.ts
@@ -1,27 +1,48 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { NGXLogger } from 'ngx-logger';
 
 import { AuthenticationService } from './authentication/authentication.service';
 import { Project } from './project.model'
+
+/**
+ * BackendError represents errors from backend
+ */
+interface BackendError {
+  trace: string;
+  error: string;
+  code: number;
+}
 
 @Injectable()
 export class BackendService {
 
   // authentication header is inserted by ngx-auth.
   private httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type':  'application/json',
-      })
-    };
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+    })
+  };
 
   constructor(
     private http: HttpClient,
     private authService: AuthenticationService,
     private logger: NGXLogger,
   ) {}
+
+  // errorLogger logs every error on backend communication
+  private errorLogger(error: HttpErrorResponse): void {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred.
+      this.logger.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      const err = <BackendError> error.error;
+      this.logger.error('Backend Service:', 'Project List', `${err.code}: ${err.error}`);
+    }
+  }
 
   // projectList lists projects of authentication user.
   // This function converts projects to Project Model
@@ -33,9 +54,11 @@ export class BackendService {
         for (const p of ps) {
           projects.push(new Project(p.name, p.id));
         }
-        this.logger.info('Backend Service:', 'Projects List', projects);
         return projects;
-      }
-    ));
+      }), tap(
+        (ps: Project[]) => {
+          this.logger.info('Backend Service:', 'Projects List', ps);
+        }, (error) => this.errorLogger(error))
+    );
   }
 }
