76ac4ed7a67805369dbd78b4a0150335c11efc9c
Parham Alvani <parham.alvani@gmail.com>
2018-10-30 17:50:21 +0330

    Logs each API call and its results.

--- ui/src/app/shared/backend.service.ts
+++ ui/src/app/shared/backend.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { NGXLogger } from 'ngx-logger';
 
 import { AuthenticationService } from './authentication/authentication.service';
 import { Project } from './project.model'
@@ -19,17 +20,20 @@ export class BackendService {
   constructor(
     private http: HttpClient,
     private authService: AuthenticationService,
+    private logger: NGXLogger,
   ) {}
 
   // projectList lists projects of authentication user.
   // This function converts projects to Project Model
   public projectsList(): Observable<Project[]> {
+    this.logger.debug('Backend Service:', 'Projects List API is called');
     return this.http.get('/api/v1/projects').pipe(map(
       (ps: any[]) => {
         const projects: Project[] = [];
         for (const p of ps) {
           projects.push(new Project(p.name, p.id));
         }
+        this.logger.info('Backend Service:', 'Projects List', projects);
         return projects;
       }
     ));
