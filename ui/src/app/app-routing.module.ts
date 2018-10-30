ef42bd2adeee7c8b4f01b5a5f662750de2e2da50
Parham Alvani <parham.alvani@gmail.com>
2018-10-30 09:18:39 +0330

    Protects the routes dude.

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,31 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, ExtraOptions } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { PublicGuard, ProtectedGuard } from 'ngx-auth';
 
-// User Login / Register
+// User Login / Register Pages
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+
+// Dashboard Pages
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 const routes: Routes = [
   { // by default client is redirected to login page.
     path: '',
-    redirectTo: '/login',
+    redirectTo: '/dashboard',
     pathMatch: 'full'
   },
   {
     path: 'login',
     component: LoginComponent,
+    canActivate: [ PublicGuard ],
     data: { title: 'Login' }
   },
   {
     path: 'register',
     component: RegisterComponent,
+    canActivate: [ PublicGuard ],
     data: { title: 'Register' }
   },
   {
     path: 'dashboard',
     component: DashboardComponent,
+    canActivate: [ ProtectedGuard ],
     data: { title: 'Dashboard' }
   },
 ];
