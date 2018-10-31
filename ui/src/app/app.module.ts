cb8e7746da46cfe2c84d4691ec4a950b316fca88
Parham Alvani <parham.alvani@gmail.com>
2018-10-31 13:26:21 +0330

    Registers google map component.

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -2,14 +2,13 @@
 import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule, Title } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AuthenticationModule, BackendService } from './shared';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { GravatarModule } from 'ngx-gravatar';
 import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
+import { AgmCoreModule } from '@agm/core';
 
 // Global Configurations
 import * as global from './globals';
@@ -20,6 +19,8 @@ import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
 import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthenticationModule, BackendService } from './shared';
 
 // Page Components
 import { LoginComponent } from './pages/login/login.component';
@@ -55,6 +56,7 @@ import { ThingNewComponent } from './pages/thing-new/thing-new.component';
     NgbModule,
     GravatarModule,
     LoggerModule.forRoot({level: NgxLoggerLevel.DEBUG}),
+    AgmCoreModule.forRoot({ apiKey: 'YOUR_KEY' }),
   ],
   providers: [ Title, BackendService ],
   bootstrap: [ AppComponent ]
