7825ba89cf22cde7c0d8a1ab60144da3fd2287f3
Parham Alvani <parham.alvani@gmail.com>
2018-10-31 08:58:30 +0330

    Organizes project pages in hierarchy.

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -9,6 +9,8 @@ import { RegisterComponent } from './pages/register/register.component';
 
 // Dashboard Pages
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ProjectDetailComponent } from './pages/project-detail/project-detail.component';
 
 const routes: Routes = [
   { // by default client is redirected to login page.
@@ -33,6 +35,23 @@ const routes: Routes = [
     component: DashboardComponent,
     canActivate: [ ProtectedGuard ],
     data: { title: 'Dashboard' }
+  },
+  {
+    path: 'projects',
+    canActivate: [ ProtectedGuard ],
+    data: { title: 'Projects' },
+    children: [
+      {
+        path: '',
+        component: ProjectsComponent,
+        data: { title: '' },
+      },
+      {
+        path: ':id',
+        component: ProjectDetailComponent,
+        data: { title: 'Project Detail' },
+      }
+    ],
   },
 ];
 
