25655ad06defa28912b6b6f84c7e5d809da6764f
Parham Alvani <parham.alvani@gmail.com>
2018-10-31 17:57:01 +0330

    Adds map for thing creation.

--- ui/src/app/pages/thing-new/thing-new.component.ts
+++ ui/src/app/pages/thing-new/thing-new.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgModel, NgForm } from '@angular/forms';
+import { MouseEvent } from '@agm/core';
 
 @Component({
   selector: 'app-thing-new',
@@ -8,9 +9,39 @@ import { NgModel, NgForm } from '@angular/forms';
 })
 export class ThingNewComponent implements OnInit {
 
+  /**
+   * Map center latitude. Map marker is also placed in this coordinates.
+   */
+  private centerLat = 35.807657;
+
+  /**
+   * Map center longitude. Map marker is also placed in this coordinates.
+   */
+  private centerLng = 51.398408;
+
   constructor() { }
 
   ngOnInit() {
+    if (navigator.geolocation) { // ask current location from the browser
+      navigator.geolocation.getCurrentPosition(this.getLocationInfo);
+    }
+  }
+
+  /**
+   * Callback function that is called when user provides its location
+   */
+  private getLocationInfo(position): void {
+    this.centerLng = position.coords.longitude;
+    this.centerLat = position.coords.latitude;
+  }
+
+  /**
+   * makerDrag is called when user map maker dragging ends.
+   * This function read the last location of the marker.
+   */
+  private markerDrag(e: MouseEvent): void {
+    this.centerLng = e.coords.lng;
+    this.centerLat = e.coords.lat;
   }
 
   /**
@@ -25,7 +56,7 @@ export class ThingNewComponent implements OnInit {
   }
 
   /**
-   * formSubmits calls when user submit the registration form.
+   * formSubmits calls when user submit the thing creation form.
    */
   private formSubmit(f: NgForm): void {
   }
