050a2f582ef711f78252393744f002d643dccd4b
Parham Alvani <parham.alvani@gmail.com>
2018-11-12 14:07:38 +0330

    Project creation page is ready.

--- src/app/pages/project-new/project-new.component.ts
+++ src/app/pages/project-new/project-new.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
-  selector: 'app-project-new',
+  selector: 'app-project-new-page',
   templateUrl: './project-new.component.html',
   styleUrls: ['./project-new.component.css']
 })
@@ -11,5 +12,20 @@ export class ProjectNewComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  /**
+   * When input is invalid in the input box, input box must truns to red this function
+   * returns true to trigger invalid class when input is invalid. use this with [class.is-invalid].
+   */
+  private isValid(m: FormControl): boolean {
+    return m.invalid && (m.dirty || m.touched);
+  }
+
+  /**
+   * formSubmits calls when user submits the project creation form.
+   */
+  private formSubmit(f: FormGroup): void {
+  }
+
 
 }
