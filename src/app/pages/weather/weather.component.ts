f10e19217143339caf7c385217beab6948e11e63
Parham Alvani <parham.alvani@gmail.com>
2018-11-12 16:01:50 +0330

    Weather component is ready in debugging.

--- src/app/pages/weather/weather.component.ts
+++ src/app/pages/weather/weather.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { BackendService } from '../../shared/backend.service';
+
 
 @Component({
   selector: 'app-weather-page',
@@ -6,10 +10,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent implements OnInit {
+  private darksky$: Observable<any[]>;
 
-  constructor() { }
+  constructor(
+    private bService: BackendService,
+  ) { }
 
   ngOnInit() {
+    this.darksky$ = this.bService.weatherDarksky(35.8064342, 51.3950481);
   }
 
 }
