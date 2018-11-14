dfe7b5822a04c21e74434e6ac880f22e7cb6a4a9
Parham Alvani <parham.alvani@gmail.com>
2018-11-14 05:43:46 +0330

    Completes health checking in side-panel.

--- src/app/side-panel/side-panel.component.ts
+++ src/app/side-panel/side-panel.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BackendService } from '../shared/backend.service';
+
 @Component({
   selector: 'app-side-panel',
   templateUrl: './side-panel.component.html',
@@ -7,27 +9,88 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidePanelComponent implements OnInit {
   private active: boolean
-  private lastCheck: Date
-  private state
 
-  constructor() {
+  private pmLastCheck: Date
+  private pmState
+
+  private wfLastCheck: Date
+  private wfState
+
+  constructor(
+    private bService: BackendService,
+  ) {
     this.active = false;
-    this.lastCheck = new Date();
-    this.state = {
+
+    // project manager
+    this.pmLastCheck = new Date();
+    this.pmState = {
       'btn-inverse': true,
       'btn-danger': false,
-      'btn-success': false
+      'btn-success': false,
+    };
+
+    // weather forecasting
+    this.wfLastCheck = new Date();
+    this.wfState = {
+      'btn-inverse': true,
+      'btn-danger': false,
+      'btn-success': false,
     };
   }
 
-  public check(): void {
-    this.lastCheck = new Date();
-    this.state = {
-      'btn-inverse': false,
-      'btn-danger': true,
-      'btn-success': false
-    };
+  public wfCheck(): void {
+    this.wfLastCheck = new Date();
+    this.bService.wfHealth().subscribe(
+      (s: boolean) => {
+        if (s === true) {
+          this.wfState = {
+            'btn-inverse': false,
+            'btn-danger': false,
+            'btn-success': true,
+          };
+        } else {
+          this.wfState = {
+            'btn-inverse': false,
+            'btn-danger': true,
+            'btn-success': false,
+          };
+        }
+      }, () => {
+        this.wfState = {
+          'btn-inverse': true,
+          'btn-danger': false,
+          'btn-success': false,
+        };
+      }
+    )
 
+  }
+
+  public pmCheck(): void {
+    this.pmLastCheck = new Date();
+    this.bService.pmHealth().subscribe(
+      (s: boolean) => {
+        if (s === true) {
+          this.pmState = {
+            'btn-inverse': false,
+            'btn-danger': false,
+            'btn-success': true,
+          };
+        } else {
+          this.pmState = {
+            'btn-inverse': false,
+            'btn-danger': true,
+            'btn-success': false,
+          };
+        }
+      }, () => {
+        this.pmState = {
+          'btn-inverse': true,
+          'btn-danger': false,
+          'btn-success': false,
+        };
+      }
+    )
   }
 
   ngOnInit() {
