8b0e17071cafbe984359e508547434e811ad0146
Parham Alvani <parham.alvani@gmail.com>
2018-11-14 06:05:32 +0330

    Adds missing semi colon.

--- src/app/shared/authentication/index.ts
+++ src/app/shared/authentication/index.ts
@@ -1,3 +1,3 @@
 export { AuthenticationModule } from './authentication.module';
 export { AuthenticationService } from './authentication.service';
-export { User } from './user.model'
+export { User } from './user.model';
