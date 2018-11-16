ab4ad675883e03ede3b3df4e94c3c01afd5d47ab
Parham Alvani <parham.alvani@gmail.com>
2018-11-17 02:56:38 +0330

    Adds sub-models of things.

--- src/app/shared/thing.model.ts
+++ src/app/shared/thing.model.ts
@@ -1,11 +1,39 @@
-export class Thing {
+export class Asset {
   constructor(
-    public name: string,
-    public id: string,
-    public model: string,
-    public latitude?: number,
-    public longitude?: number,
+    public name: string, // e.g. 100
+    public title: string, // e.g. temperature
+    public kind: string, // e.g. number
+    public type: string, // sensor
   ) {
+  }
+}
+
+export class Connectivity {
+}
+
+export class Thing {
+  public name: string;
+  public id: string;
+  public model: string;
+  public latitude: number;
+  public longitude: number;
+  public assets: Array<Asset> = [];
+  public tokens: Array<string> = [];
+
+  constructor(input: any) {
+    this.name = input.name;
+    this.id = input.id;
+    this.model = input.model;
+    this.latitude = input.location.coordinates[1];
+    this.longitude = input.location.coordinates[0];
+
+    for (let name in input.assets) {
+      this.assets.push();
+    }
+
+    for (let token of input.tokens) {
+      this.tokens.push(token);
+    }
   }
 
 }
