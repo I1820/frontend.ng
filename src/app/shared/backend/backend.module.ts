3939f974967aa7006d2c76920e0debc9907abf47
Parham Alvani <parham.alvani@gmail.com>
2018-11-21 05:43:07 +0330

    Adds comments about backend module.

--- src/app/shared/backend/backend.module.ts
+++ src/app/shared/backend/backend.module.ts
@@ -5,6 +5,12 @@ import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
 
 import { LoggerInterceptor } from './logger';
 
+/**
+ * BackendModule sends and receives requests with I1820 backend component.
+ * Authentication handles by ngx-auth module and content type header is added by I1820 frontend proxy.
+ * Each function in this service handles errors to show them in logs and with toasts but it throw them too so
+ * caller can handle them again.
+ */
 @NgModule({
   imports: [
     CommonModule,
