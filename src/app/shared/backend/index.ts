94f9afcc1da0284c5dcd719043c2714c5ceac4df
Parham Alvani <parham.alvani@gmail.com>
2018-11-21 05:41:47 +0330

    Exports backend models and services.

--- src/app/shared/backend/index.ts
+++ src/app/shared/backend/index.ts
@@ -1,5 +1,8 @@
 export { BackendModule } from './backend.module';
-export { BackendService } from './backend.service';
+export { ProjectService } from './project.service';
+export { ThingService } from './thing.service';
 export { HealthService } from './health.service';
 export { WeatherService } from './weather.service';
 export { Darksky } from './darksky.model';
+export { Project } from './project.model';
+export { Thing } from './thing.model';
